f34a850ac0535a8d50b160753562e7728807f0cf
Hackbright Student <user@mini-002.(none)>
2014-02-04 12:02:45 -0800

    number of tries works too

--- guessing.py
+++ guessing.py
@@ -9,14 +9,17 @@ print "%s, i'm thinking of a number between 1 and 100. try to guess my number." 
 
 secret_number = 40
 input_number = 1
+guesses = 0
 while input_number != secret_number:
-    print "At the top input_number %d" % input_number
+ #   print "At the top input_number %d" % input_number
     input_number = int(raw_input())
+    guesses += 1
+    print guesses
     if input_number > secret_number:
         print ("Ha Ha that's too big!")
     elif input_number < secret_number:
-        print ("Loser -- that's too small!")
+        print ("Tee hee  -- that's too small!")
 
-print( "You got it %s I was thinking of %d") % (name, secret_number)
+print( "You got it %s I was thinking of %d .  It took you %d tries") % (name, secret_number, guesses)
 
 
