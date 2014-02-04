97556c595447a1edb770fcf6ca16b85e1cd9d6e0
Hackbright Student <user@mini-002.(none)>
2014-02-04 11:56:49 -0800

    guessing special number works

--- guessing.py
+++ guessing.py
@@ -2,6 +2,21 @@
 
 print "What is your name?"
 name = raw_input()
-print "Hello %s" % name
+#print "Hello %s" % name
+#print type(name) #check type of our input
 
-print type(name) #check type of our input
+print "%s, i'm thinking of a number between 1 and 100. try to guess my number." % name
+
+secret_number = 40
+input_number = 1
+while input_number != secret_number:
+    print "At the top input_number %d" % input_number
+    input_number = int(raw_input())
+    if input_number > secret_number:
+        print ("Ha Ha that's too big!")
+    elif input_number < secret_number:
+        print ("Loser -- that's too small!")
+
+print( "You got it %s I was thinking of %d") % (name, secret_number)
+
+
